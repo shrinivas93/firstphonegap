68a19cf8a79fc444fe8838ceddb592f7ed024755
Shrinivas Shukla <ssshukla1993@gmail.com>
2016-09-21 16:42:05 +0530

    * Added Jquery * Added sample Ajax call

--- js/script.js
+++ js/script.js
@@ -2,5 +2,10 @@ window.onload = function() {
 	var btn= document.getElementById("click");
 	btn.addEventListener('click', function() {
 		alert("Clicked");
+		$.ajax('http://jsonplaceholder.typicode.com/posts/1',{
+			success: function(data) {
+				alert(data.title);
+			}
+		});
 	});
 };
